cf083ca7fc6c2a748495efca24916add263f4d07
jenifaa <jefrahman32@gmail.com>
2025-01-16 04:57:10 +0600

    added mongodb and userCollection

--- index.js
+++ index.js
@@ -8,6 +8,51 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5lka3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"`
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    const parcelCollection = client.db("parcelDb").collection("Parcel");
+    const userCollection = client.db("parcelDb").collection("users");
+    app.post("/users", async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email };
+        const existingUser = await userCollection.findOne(query);
+        if (existingUser) {
+          return res.send({ message: "user already exist", insertedId: null });
+        }
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+      });
+  
+      
+  
+      app.get("/users", async (req, res) => {
+        const result = await userCollection.find().toArray();
+        res.send(result);
+      });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get("/", (req, res) => {
     res.send("Parcel is coming");
